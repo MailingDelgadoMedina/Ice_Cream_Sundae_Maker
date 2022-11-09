ac5ce93c70d3bc48b642def483f4f89f9ac0dc71
MailingDelgadoMedina <mailing.delgado.scitexas@gmail.com>
2022-11-09 00:39:49 -0500

    finished front end its not wired up yet

--- app/src/main/java/com/mailingdelgadomedina/icecreamsundaemaker/MainActivity.java
+++ app/src/main/java/com/mailingdelgadomedina/icecreamsundaemaker/MainActivity.java
@@ -1,7 +1,10 @@
 package com.mailingdelgadomedina.icecreamsundaemaker;
 
+
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.widget.SeekBar;
 import android.widget.TextView;
@@ -9,18 +12,19 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 SeekBar seekbar;
-TextView textView;
+TextView hotFudgeTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         seekbar = findViewById(R.id.seekBar);
-        textView = findViewById(R.id.textView);
+        hotFudgeTextView = findViewById(R.id.hotfudge_oz_textView);
         seekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @SuppressLint("SetTextI18n")
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 Toast.makeText(MainActivity.this,"Progress Updated", Toast.LENGTH_SHORT).show();
-            textView.setText(Integer.toString(progress));
+            hotFudgeTextView.setText(Integer.toString(progress)+"oz");
             }
 
             @Override
